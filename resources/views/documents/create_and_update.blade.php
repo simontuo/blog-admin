d13a815a3e885a10aec3e45118be654588893b6f
simontuo <simontuo666@gmail.com>
2019-06-11 02:43:54 +0000

    fix document update

--- resources/views/documents/create_and_update.blade.php
+++ resources/views/documents/create_and_update.blade.php
@@ -6,8 +6,8 @@
     </div>
     <div class="show-body p-3">
         <document-create-and-update-form
-                @if(\Route::currentRouteName() == 'resources.edit')
-                resource="{{ $resource->toJson() }}"
+                @if(\Route::currentRouteName() == 'documents.edit')
+                document="{{ $document->toJson() }}"
                 @endif
                 tags="{{ $tags->toJson() }}">
         </document-create-and-update-form>
