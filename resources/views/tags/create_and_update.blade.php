93645ca454088345d474955ae7e29bfe39472126
simontuo <simontuo666@gmail.com>
2019-06-15 09:21:02 +0000

    fix show page

--- resources/views/tags/create_and_update.blade.php
+++ resources/views/tags/create_and_update.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.iframe')
 
 @section('content')
-    <div class="bread-crumb">
-        <bread-crumb breadcrumbs="{{ collect($breadcrumbs)->toJson() }}"></bread-crumb>
-    </div>
+{{--    <div class="bread-crumb">--}}
+{{--        <bread-crumb breadcrumbs="{{ collect($breadcrumbs)->toJson() }}"></bread-crumb>--}}
+{{--    </div>--}}
     <div class="show-body p-3">
         <tag-create-and-update-form
             @if(\Route::currentRouteName() == 'tags.edit')
